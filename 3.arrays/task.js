247f6ea484aac1548f8768ff71ad021e95af6e20
DANIL <Pikalov_danil@mail.ru>
2023-08-07 22:10:54 +0500

    Домашнее задание к лекции 3 «Массивы»

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -12,26 +12,27 @@ let check = arr1.length === arr2.length && arr1.every(function(elem, index) {
 
 // Задача - 2
 function getUsersNamesInAgeRange(users, gender) {
-  	// Проверяем входящий массив на пустоту
-  	if(users.length == 0) {
-   		return 0;
-  	}
-  	// Выбираем элементы с нужным гендером
-  	let selectGender = users.filter(item => item.gender == gender);
+  // Проверяем входящий массив на пустоту
+  if(users.length == 0) {
+   	return 0;
+  }
+  
+  // Выбираем элементы с нужным гендером
+  let selectGender = users.filter(item => item.gender == gender);
 
-  	// Если элементов нет, то return = 0
-  	if(selectGender == 0) {
-    	return 0;
-  	}
+  // Если элементов нет, то return = 0
+  if(selectGender == 0) {
+    return 0;
+  }
 
-  	// формируем массив только их возраста
-  	let selectAge = selectGender.map(item =>item.age);
+  // формируем массив только их возраста
+  let selectAge = selectGender.map(item =>item.age);
 
-  	// суммируем возраст
-  	let sumAge = selectAge.reduce((sum, current) => sum + current);
+  // суммируем возраст
+  let sumAge = selectAge.reduce((sum, current) => sum + current);
   	
-  	// Считаем средний возраст
-  	let result = sumAge / selectAge.length;
+  // Считаем средний возраст
+  let result = sumAge / selectAge.length;
 
  	return result;
 }
